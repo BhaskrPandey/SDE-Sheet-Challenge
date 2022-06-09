b8c9c8237339938ba83923b4b6c0a7399a7ba37d
BhaskrPandey <106923489+BhaskrPandey@users.noreply.github.com>
2022-06-09 23:08:10 +0530

    Add files via upload

---
+++ Arrays/pascal_Triangle.java
@@ -0,0 +1,27 @@
+package ARRAYS;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class pascal_Triangle {
+
+    public List<List<Integer>> generate(int numRows){
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<Integer> row, pre = null;
+        for (int i = 0; i< numRows; ++i){
+            row = new ArrayList<Integer>();
+            for (int j =0; i<=i; ++j)
+                if (j==0 || j==i)
+                    row.add(1);
+                else
+                    row.add(pre.get(j-1) + pre.get(j));
+                pre = row;
+                res.add(row);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
