c7fe593659a666242c7c09cd49c73a645a532656
BhaskrPandey <106923489+BhaskrPandey@users.noreply.github.com>
2022-06-10 08:48:44 +0530

    Update StockBuyandSell.java

--- Arrays/StockBuyandSell.java
+++ Arrays/StockBuyandSell.java
@@ -15,3 +15,8 @@ public class StockBuyandSell {
         return maxProfit;
     }
 }
+public static void main(String[] args) {
+        int arr[] = {7,1,5,3,6,4};
+        int maxProfit = maxProfit(arr);
+        System.out.println("Max profit is: " + maxPro);
+    }
