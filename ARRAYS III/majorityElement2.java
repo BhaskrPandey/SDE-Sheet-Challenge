c1a70995fbbc208fad2715827997eaf9e4deefec
BhaskrPandey <106923489+BhaskrPandey@users.noreply.github.com>
2022-06-16 08:50:15 +0530

    Add files via upload

---
+++ ARRAYS III/majorityElement2.java
@@ -0,0 +1,20 @@
+package ArraysIII;
+
+public class majorityElement2 {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            if(num==candidate)
+                count += 1;
+            else count -= 1;
+        }
+
+        return candidate;
+    }
+
+}
